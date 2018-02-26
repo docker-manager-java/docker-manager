40c73ac5fb86bc1d2c0cddb0d94909c0bcf1ebfa
cc-man <26114619@qq.com>
2018-02-26 17:39:56 +0800

    cc-man "update"
    add dockerclient test

--- docker.manager.handle/src/test/java/com/musket/docker/manager/dockerclient/DockerClientConnect.java
+++ docker.manager.handle/src/test/java/com/musket/docker/manager/dockerclient/DockerClientConnect.java
@@ -1,11 +1,15 @@
 package com.musket.docker.manager.dockerclient;
 
 import com.musket.docker.manager.dockerclientapi.connect.DokerConnect;
+import com.musket.docker.manager.dockerclientapi.connect.interfaces.DockerClientApi;
 import com.spotify.docker.client.DockerClient;
 import com.spotify.docker.client.exceptions.DockerException;
 import com.spotify.docker.client.messages.Image;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.junit.Before;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.List;
 
@@ -13,26 +17,32 @@ import java.util.List;
  * Created by Administrator on 2018/2/26.
  */
 public class DockerClientConnect extends TestCase {
-
+    private static DokerConnect dockerConnect;
+    private static DockerClientApi dockerClientApiImpl;
     public static junit.framework.Test suite() {
         TestSuite suite = new TestSuite();
+        suite.addTest(new DockerClientConnect("start"));
         suite.addTest(new DockerClientConnect("testConnectDockerClient"));
         return suite;
     }
+
+    @Before
+    public void start() throws Exception{
+        BeanFactory factory = new ClassPathXmlApplicationContext("applicationContext.xml");
+        dockerConnect = (DokerConnect) factory.getBean("dockerConnect");
+        dockerClientApiImpl = (DockerClientApi) factory.getBean("dockerClientApiImpl");
+    }
+
+
 
     public DockerClientConnect(String name) {
         super(name);
     }
 
     public void testConnectDockerClient(){
-        DockerClient docker =  DokerConnect.connect("http://192.168.46.132:2375/");
-        try {
-            final List<Image> quxImages = docker.listImages();
+        DockerClient docker =  dockerConnect.connect();
+            final List<Image> quxImages = dockerClientApiImpl.listImanges(docker);
             assertNotNull(quxImages);
-        } catch (DockerException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+
     }
 }
