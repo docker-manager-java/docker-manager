25749623d9438e17b1c81181b1859a0344cd393a
cc-man <26114619@qq.com>
2018-02-28 14:27:47 +0800

    cc-man "update"

--- docker.manager.handle/src/main/java/com/musket/docker/manager/dockerclientapi/connect/impl/DockerClientApiImpl.java
+++ docker.manager.handle/src/main/java/com/musket/docker/manager/dockerclientapi/connect/impl/DockerClientApiImpl.java
@@ -17,6 +17,7 @@ import java.util.Map;
  */
 public class DockerClientApiImpl implements DockerClientApi {
 
+    
     @Override
     public List<ImageSearchResult> searchImages(DockerClient dockerClient, String name) {
         List<ImageSearchResult> list = null;
