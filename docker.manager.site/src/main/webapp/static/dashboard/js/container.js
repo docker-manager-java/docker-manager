c01bc5ec69340c50e99ddf2d6bc56999ae0e2205
cc-man <261145619@qq.com>
2018-03-06 18:22:46 +0800

    cc-man "update" add deletemodel

--- docker.manager.site/src/main/webapp/static/dashboard/js/container.js
+++ docker.manager.site/src/main/webapp/static/dashboard/js/container.js
@@ -1,6 +1,8 @@
 /**
  * Created by Administrator on 2018/3/4.
  */
+var success = "<span class='label label-success'>" +"SUCCESS"+"</span>";
+var danger ="<span class='label label-danger'>"+"DANGER"+"</span>"
 //创建compose项目
 
 $(function(){
@@ -11,7 +13,7 @@ $("#creatfromyaml").click(function(){
     var  name = $("#template_name").val();
   // var p = ReplaceSeperator(s);
     $.ajax({
-        url:"/engine/compose/creatfromyaml",
+        url:"/engine/compose/model/creatfromyaml",
         method: "POST",
         dataType: "text",
         data: {"yaml":yamltext,"name":name},
@@ -27,7 +29,7 @@ $("#creatfromyaml").click(function(){
 
 function loadlist(){
     $.ajax({
-        url:"/engine/compose/listprojects",
+        url:"/engine/compose/model/listmodels",
         method: "GET",
         dataType: "json",
         success : function(data){
@@ -36,18 +38,52 @@ function loadlist(){
            var p = obj.projects;
             var tr = "";
             var n =1;
+            var operation_start = "<button type='button' class='btn btn-danger' onclick='deletemodel(this)'><i class='fa fa-trash-o'></i> Danger</button>";//data-toggle='modal' data-target='#myModal_Prompt'
+            var ind;
 
-            var obj = {};
             for(var k in p) {
+                ind = inspectprojects(k)?success:danger
                 tr += "<tr>" +
                     "<td>" + n++ + "</td>" +
                     "<td>" + k + "</td>" +
                     "<td>" + p[k] + "</td>" +
+                    "<td>" + ind  +"</td>" +
+                    "<td>" + operation_start  +"</td>" +
                     "</tr>";
             }
             $("#project-list tbody").html(tr);
         }
     })
 
+}
+var b ;
+function inspectprojects(name){
+
+    $.ajax({
+        url:"/engine/compose/model/inspectprojects",
+        method: "GET",
+        dataType: "json",
+        async:false,
+        data: {"name":name},
+        success : function(data){
+           b = data.resultInfo.success;
+        }
+    })
+    return b;
+
+}
+function deletemodel(element){
+    var name = $(element).parent().parent().children()[1].innerText;
+    $.ajax({
+        url:"/engine/compose/model/deletemodelbyname",
+        method: "GET",
+        dataType: "json",
+        async:false,
+        data: {"name":name},
+        success : function(data){
+            $ ("#btn-success").click();
+           loadlist();
+        }
+    })
 }
 
