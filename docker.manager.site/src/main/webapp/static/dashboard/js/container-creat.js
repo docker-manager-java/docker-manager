a26c5cc1d38152e65d707d4509a3f398ba3f6595
cc-man <261145619@qq.com>
2018-04-15 21:41:53 +0800

    add creat_submit（）

--- docker.manager.site/src/main/webapp/static/dashboard/js/container-creat.js
+++ docker.manager.site/src/main/webapp/static/dashboard/js/container-creat.js
@@ -44,8 +44,46 @@ $("#creat").click(function(){
         "</tr>";
     $("#close_creat").click();
     $("#app_add_table").append(trHTML);
-
-
 })
 
+
+$("#creat_submit").click(function(){
+    var creat_name = $("#creat_name").val();
+    var creat_ip = $("#creat_ip").val();
+    var creat_ver = $("#creat_ver").val();
+    var datas ={};
+    datas.name =creat_name;
+    datas.ip = creat_ip;
+    datas.ver  =creat_ver;
+    datas.data = {};
+    var tb=document.getElementById("app_add_table");    //获取table对像
+    var rows=tb.rows;
+    var list_0 =[]//记录列名称
+    var list = [];//记录行级成员
+    for(var i=0;i<rows.length;i++){    //--循环所有的行
+        var list_obj ={};
+        var map_=new Map();
+        var cells=rows[i].cells;
+        for(var j=1;j<cells.length;j++){   //--循环所有的列
+            if(i === 0){
+                list_0.push(cells[j].innerHTML)
+            }else{
+                list_obj[[list_0[j-1]]] =cells[j].innerHTML
+            }
+        }
+        if(i != 0){
+        list.push(list_obj)
+        }
+    }
+    datas.data ="{ data:"+JSON.stringify(list)+"}";
+    $.ajax({
+        url:"/engine/compose/model/creat",
+        method: "POST",
+        dataType: "json",
+        data:datas,
+        success : function(data){
+        }
+    })
+
+})
 
